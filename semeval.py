1d2dec2a3a9136e0d1574ba0fa13340b3a50cbc8
Clare Arrington <arrinj@rpi.edu>
2021-11-04 12:17:53 -0400

    Changed hardcoded path to data_path

--- semeval.py
+++ semeval.py
@@ -2,13 +2,14 @@
 from base_experiment import main, Target_Info, Train_Method_Info
 import pandas as pd
 
-target_data = f'/home/clare/Data/masking_results/semeval/ccoha1_extra/target_sense_labels.csv'
-target_data = pd.read_csv(target_data, usecols=['target'])
-all_targets = [t for t in target_data.target.unique()]
+data_path = '/home/clare/Data'
+# target_data = f'{data_path}/masking_results/semeval/ccoha1_extra/target_sense_labels.csv'
+# target_data = pd.read_csv(target_data, usecols=['target'])
+# all_targets = [t for t in target_data.target.unique()]
+# skip = []
 
 targets = []
-skip = []
-with open('/home/clare/Data/corpus_data/semeval/truth/binary.txt') as fin:
+with open(f'{data_path}/corpus_data/semeval/truth/binary.txt') as fin:
     og_targets = fin.read().strip().split('\n')
     for target in og_targets:
         target, label = target.split('\t')
@@ -17,20 +18,20 @@ with open('/home/clare/Data/corpus_data/semeval/truth/binary.txt') as fin:
 
         target = Target_Info(word=word, shifted_word=target, is_shifted=label)
         targets.append(target)
-        skip.append(word)
+        # skip.append(word)
 
-for target in all_targets:
-    if target in skip:
-        continue
-    target = Target_Info(word=target, shifted_word=target, is_shifted=None)
-    targets.append(target)
+# for target in all_targets:
+#     if target in skip:
+#         continue
+#     target = Target_Info(word=target, shifted_word=target, is_shifted=None)
+#     targets.append(target)
         
 print(f'{len(targets)} targets loaded')
 
 ## Data Info
 dataset_name = 'semeval'
-anchor_info  = ('ccoha2', 'CCOHA 1960 - 2010')
-align_info   = ('ccoha1_extra', 'CCOHA 1810 - 1860')
+anchor_info = ('ccoha2', 'CCOHA 1960 - 2010')
+align_info  = ('ccoha1', 'CCOHA 1810 - 1860')
 
 #%%
 # Results from paper
@@ -54,22 +55,24 @@ cos_classify_params = { "rate": 1.5,
                 "n_negatives": 100}
 
 align_methods = [
-    #Train_Method_Info('global', None)
+    #Train_Method_Info('global', None),
     Train_Method_Info('s4', s4_align_params)
 ]
 
 classify_methods = [
     Train_Method_Info('cosine', cos_classify_params, 0)
-    # Train_Method_Info('s4', s4_classify_params, .5)
+    #Train_Method_Info('s4', s4_classify_params, .5)
 ]
 
 #%%
 vector_types = ['sense']
 
-main(dataset_name, targets,
-     align_info, anchor_info,
-     vector_types,
-     align_methods, classify_methods,
-     num_loops=10)
+main(
+    dataset_name, data_path,
+    targets,
+    align_info, anchor_info,
+    vector_types,
+    align_methods, classify_methods,
+    num_loops=10)
 
 # %%
