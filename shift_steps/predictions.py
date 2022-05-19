db3e53e02941852c54cb16a4e93909b474a5b562
Clare Arrington <arrinj@rpi.edu>
2022-05-19 11:15:08 -0400

    Added both side prediction and modified single side

--- shift_steps/predictions.py
+++ shift_steps/predictions.py
@@ -4,8 +4,9 @@ import pandas as pd
 from typing import NamedTuple, List
 from collections import defaultdict
 
-def assess_standard_prediction(predictions, threshold: float, targets: List[NamedTuple]):
-    target_words, _, true_labels = list(zip(*targets))    
+def assess_standard_prediction(predictions, threshold: float, target_labels):
+    true_labels = list(target_labels.values())
+    target_words = [target for target, anchor in target_labels.keys()]
 
     predictions = predictions > threshold
     accuracy = accuracy_score(true_labels, predictions)
@@ -30,80 +31,205 @@ def get_prediction_info(predictions, threshold, align_model, anchor_model, word_
         word_count = align_model.wv.get_vecattr(sense_word, "count")
         anchor_wc = anchor_model.wv.get_vecattr(anchor_word, "count")
         
-        info = (anchor_word, word_count, anchor_wc, dist, shift_prediction)
+        info = (sense_word, word_count, anchor_wc, dist, shift_prediction)
         target = sense_word.split('.')[0]
 
-        prediction_info[target][sense_word].append(info)
+        prediction_info[target][anchor_word].append(info)
 
     return prediction_info
 
-def make_sense_prediction(prediction_info, threshold):
+def get_weighted_shift(predictions, threshold, use_other_count=False):
+    shifted_count = 0
+    unshifted_count = 0
+    weighted_dist = 0
+    for sense, count, other_count, dist, shift_prediction in predictions:
+        if use_other_count:
+            count = other_count
+        weighted_dist += (dist * count)
+        if shift_prediction:
+            shifted_count += count
+        else:
+            unshifted_count += count
 
-    ratios = [.05, .1, .15, .2]
-    ratio_data = defaultdict(list)
-    shift_data = {shift : [] for shift in ['Majority', 'Main', 'Weighted']}
+    weighted_dist /= (shifted_count + unshifted_count)
+    return weighted_dist > threshold
 
-    target_words = []
-    true_labels = []
+def make_normal_prediction(prediction_info, threshold):
+    # ratios = [.05, .1, .15, .2]
+    # ratio_data = defaultdict(dict)
+    shift_data = {shift : {} for shift in [ 'Majority', 'Main', 'Weighted']}
 
     for target, sense_predictions in prediction_info.items():
-        for sense, predictions in sense_predictions.items():
-
-            target_words.append(sense)
-            true_labels.append(True)
-
-            shifted_count = 0
-            unshifted_count = 0
-            weighted_dist = 0
+        for anchor, predictions in sense_predictions.items():
+            pair = (target, anchor)
             shifts = []
-
-            for anchor, count, anch_c, dist, shift_prediction in predictions:
-                weighted_dist += (dist * count)
-
-                shifts.append(shift_prediction)
-                if shift_prediction:
-                    shifted_count += count
-                else:
-                    unshifted_count += count
 
             majority_cutoff = len(shifts) // 2
             is_shifted = sum(shifts) > majority_cutoff
-            shift_data['Majority'].append(is_shifted)
+            shift_data['Majority'][pair] = is_shifted
 
             biggest_sense = max(predictions, key=lambda t: t[1])
             is_shifted = biggest_sense[4]
-            shift_data['Main'].append(is_shifted)
+            shift_data['Main'][pair] = is_shifted
 
-            weighted_dist /= (shifted_count + unshifted_count)
-            is_shifted = weighted_dist > threshold
-            shift_data['Weighted'].append(is_shifted)
+            is_shifted = get_weighted_shift(predictions, threshold)
+            shift_data['Weighted'][pair] = is_shifted
 
-            ratio = shifted_count / (shifted_count + unshifted_count)
-            for ratio_cutoff in ratios:
-                is_shifted = ratio >= ratio_cutoff
-                ratio_cutoff = f'{int(ratio_cutoff*100)}%'
-                ratio_data[ratio_cutoff].append(is_shifted)
+            # ratio = shifted_count / (shifted_count + unshifted_count)
+            # for ratio_cutoff in ratios:
+            #     is_shifted = ratio >= ratio_cutoff
+            #     ratio_cutoff = f'{int(ratio_cutoff*100)}%'
+            #     ratio_data[ratio_cutoff][pair] = is_shifted
 
-    return shift_data, ratio_data, target_words, true_labels
+    return shift_data, None #, ratio_data
 
-def assess_sense_prediction(shift_data, ratio_data, target_words, true_labels):
-    
-    # target_words, _, true_labels = list(zip(*target_word_pairs)) 
+def assess_normal_prediction(shift_data, ratio_data, target_labels):
+    true_labels = list(target_labels.values())
+    target_words = [target for target, anchor in target_labels.keys()]
 
     accuracies = {}
+    method_preds = {}
     for method, shift_pred in shift_data.items():
-        accuracy = accuracy_score(true_labels, shift_pred)
+        pred_labels = list(shift_pred.values())
+        accuracy = accuracy_score(true_labels, pred_labels)
         #print(f'{method} accuracy: {accuracy:.2f}')
         accuracies[method] = accuracy
+        method_preds[method] = pred_labels
 
-    for ratio_cutoff, shift_pred in ratio_data.items():
-        accuracy = accuracy_score(true_labels, shift_pred)
-        #print(f'{ratio_cutoff} ratio accuracy: {accuracy:.2f}')
-        accuracies[ratio_cutoff] = accuracy
+    # ratio_preds = {}
+    # for ratio_cutoff, shift_pred in ratio_data.items():
+    #     pred_labels = list(shift_pred.values())
+    #     accuracy = accuracy_score(true_labels, pred_labels)
+    #     #print(f'{ratio_cutoff} ratio accuracy: {accuracy:.2f}')
+    #     accuracies[ratio_cutoff] = accuracy
+    #     ratio_preds[ratio_cutoff] = pred_labels
 
     data = {'Words' : target_words, 'True Label' : true_labels}
-    data.update(shift_data)
-    data.update(ratio_data)
+    data.update(method_preds)
+    # data.update(ratio_preds)
+    results = pd.DataFrame(data)
+
+    for label in results.columns:
+        if label != 'Words':
+            results[label] = results[label].astype(int).map({0:'No Shift', 1:'Shifted'})
+
+    return accuracies, results
+
+def make_sense_prediction(prediction_info, threshold):
+    pred_labels = {shift : {} for shift in ['Majority', 'Main', 'Weighted']}
+
+    sense_matched_data = {}
+
+    for target, sense_predictions in prediction_info.items():
+        # target = 'face'
+        # sense_predictions = prediction_info[target]
+        # TODO: this is bad for US / UK
+        pair = (target, target)
+        align_matches = {}
+        anchor_matches = {}
+        for anchor_sense in sorted(sense_predictions):     
+            predictions = sorted(sense_predictions[anchor_sense])
+            closest_match = min(predictions, key = lambda t: t[3])
+            anchor_matches[anchor_sense] = closest_match
+
+            for align_tuple in predictions:
+                align_sense, *tuple_info = align_tuple
+                anchor_tuple = tuple([anchor_sense] + tuple_info)
+                if align_sense not in align_matches:
+                    closest_match = anchor_tuple
+                else:
+                    closest_match = min(
+                        align_matches[align_sense], anchor_tuple, key = lambda t: t[3])
+                align_matches[align_sense] = closest_match
+
+        sense_matched_data[target] = (anchor_matches, align_matches)
+
+        preds = [anchor_matches.values(), align_matches.values()]
+
+        ## For each of these below, assume not shifted until we find a case.
+        ## Get majority shift
+        is_shifted = False
+        for predictions in preds:
+            shifts = [m[-1] for m in predictions]
+            majority_cutoff = len(shifts) // 2
+            is_shifted = is_shifted or (sum(shifts) > majority_cutoff)
+        pred_labels['Majority'][pair] = is_shifted
+
+        ## Get main shift
+        is_shifted = False
+        for predictions in preds:
+            biggest_sense = max(predictions, key=lambda t: t[1])
+            is_shifted = is_shifted or biggest_sense[4]        
+        pred_labels['Main'][pair] = is_shifted
+
+        ## Get weighted shift
+        anchor_shift = get_weighted_shift(
+            anchor_matches.values(), threshold)
+        align_shift = get_weighted_shift(
+            align_matches.values(), threshold, use_other_count=True)
+
+        is_shifted = align_shift or anchor_shift
+        pred_labels['Weighted'][pair] = is_shifted
+
+    return pred_labels, sense_matched_data
+
+def assess_sense_prediction(pred_labels, target_labels):
+    true_labels = list(target_labels.values())
+    target_words = [target for target, anchor in target_labels.keys()]
+
+    accuracies = {}
+    method_preds = {}
+    for method, shift_pred in pred_labels.items():
+        pred_labels = list(shift_pred.values())
+        accuracy = accuracy_score(true_labels, pred_labels)
+        accuracies[method] = accuracy
+        method_preds[method] = pred_labels
+
+    data = {'Words' : target_words, 'True Label' : true_labels}
+    data.update(method_preds)
+    results = pd.DataFrame(data)
+
+    for label in results.columns:
+        if label != 'Words':
+            results[label] = results[label].astype(int).map({0:'No Shift', 1:'Shifted'})
+
+    return accuracies, results
+
+def make_shared_prediction(prediction_info, threshold):
+    shift_data = {shift : {} for shift in [ 'Majority', 'Main', 'Weighted']}
+
+    for target, sense_predictions in prediction_info.items():
+        for anchor, predictions in sense_predictions.items():
+            pair = (target, anchor)
+            shifts = []
+
+            majority_cutoff = len(shifts) // 2
+            is_shifted = sum(shifts) > majority_cutoff
+            shift_data['Majority'][pair] = is_shifted
+
+            biggest_sense = max(predictions, key=lambda t: t[1])
+            is_shifted = biggest_sense[4]
+            shift_data['Main'][pair] = is_shifted
+
+            is_shifted = get_weighted_shift(predictions, threshold)
+            shift_data['Weighted'][pair] = is_shifted
+
+    return shift_data
+
+def assess_shared_prediction(shift_data, target_labels):
+    true_labels = list(target_labels.values())
+    target_words = [target for target, anchor in target_labels.keys()]
+
+    accuracies = {}
+    method_preds = {}
+    for method, shift_pred in shift_data.items():
+        pred_labels = list(shift_pred.values())
+        accuracy = accuracy_score(true_labels, pred_labels)
+        accuracies[method] = accuracy
+        method_preds[method] = pred_labels
+
+    data = {'Words' : target_words, 'True Label' : true_labels}
+    data.update(method_preds)
     results = pd.DataFrame(data)
 
     for label in results.columns:
