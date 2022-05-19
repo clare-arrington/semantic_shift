a6874595474f002893f603809353c86d1e8e900c
Clare Arrington <arrinj@rpi.edu>
2022-05-19 11:16:42 -0400

    Added word pairs class and changed checks

--- shift_steps/target_prep.py
+++ shift_steps/target_prep.py
@@ -4,8 +4,6 @@ from typing import NamedTuple, List
 from collections import namedtuple
 import re
 
-Target_Info = namedtuple('Target', ['word', 'shifted_word', 'is_shifted'])
-
 ## Parse through the targets to match them up 
 def make_word_pairs(
     targets: List[NamedTuple], 
@@ -14,44 +12,57 @@ def make_word_pairs(
 
     all_wp = []
     target_wp = []
-    if align_wv.type in ['original', 'normal', 'both_sense']:
+    target_labels = {}
+
+    if align_wv.type in ['original', 'normal']:
         for target in targets:
-            all_wp.append((target.word, target.shifted_word))
+            pair = (target.word, target.shifted_word)
+            all_wp.append(pair)
             
-            ## TODO: double check the logic of this
             if target.is_shifted is not None:
-                target_wp.append((target.word, target.shifted_word))
-
+                target_wp.append(pair)
+                target_labels[pair] = target.is_shifted
     else:
-        ## TODO: verify this is okay for SSA still
-        print(f'{len(align_wv.senses)} filtered senses for {align_wv.desc}')
-        print(f'{len(anchor_wv.senses)} filtered senses for {anchor_wv.desc}\n')
+        print(f'{len(align_wv.senses)} filtered senses for {align_wv.type} align WV {align_wv.desc}')
+        print(f'{len(anchor_wv.senses)} filtered senses for {anchor_wv.type} anchor WV {anchor_wv.desc}\n')
 
+        # TODO: this doesn't seem like it'd work for US / UK
         for target in targets:
+            # Find the senses for the current align target
             r = re.compile(f'{target.word}.[0-9]')
             align_sense_matches = filter(r.match, align_wv.senses)
 
-            if anchor_wv.type == 'sense':
-                anchor_sense_matches = filter(r.match, anchor_wv.senses)
-            else:
+            # Find the senses for the current anchor match if it exists
+            if anchor_wv.type == 'normal':
+                # Sense paired to plain token
                 anchor_sense_matches = [target.shifted_word]
+            elif anchor_wv.type == 'shared_sense':
+                # Sense paired to itself on other side
+                anchor_sense_matches = []
+            elif anchor_wv.type == 'sense':
+                # Sense paired to all senses
+                anchor_sense_matches = filter(r.match, anchor_wv.senses)
 
             for sense, match in itertools.product(align_sense_matches, anchor_sense_matches):
-                all_wp.append((sense, match))
+                pair = (sense, match)
+                all_wp.append(pair)
 
                 if target.is_shifted is not None:
-                    target_wp.append((sense, match))
+                    target_wp.append(pair)
+
+            if target.is_shifted is not None:
+                target_labels[(target.word, target.shifted_word)] = target.is_shifted
 
     print(f'{len(all_wp)} total word pairs; {len(target_wp)} of those are targets')
 
-    return all_wp, target_wp
+    return Word_Pairs(all_wp, target_wp, target_labels)
 
-def check_wv_target(wv, t_word):
+def check_target_in_wv(wv, t_word):
     if wv.type == 'sense' and t_word not in wv.terms_with_sense:
-        print(f'{t_word} senses missing from {wv.corpus_name}')
+        print(f'Check 1: {t_word} senses missing from {wv.corpus_name}')
         return True
     elif wv.type == 'normal' and t_word not in wv.normal_vec.words:
-        print(f'{t_word} missing from {wv.corpus_name}')
+        print(f'Check 1: {t_word} missing from {wv.corpus_name}')
         return True
     else:
         return False
@@ -67,23 +78,22 @@ def filter_targets(
     for index, target in enumerate(targets):
 
         ## Check for the align WV
-        if check_wv_target(align_wv, target.word):
+        if check_target_in_wv(align_wv, target.word):
+            print(align_wv.corpus_name, align_wv.type, target.word)
             remove_targets.append(index)
             continue
 
         ## Check for the anchor WV
-        if check_wv_target(anchor_wv, target.shifted_word):
-            remove_targets.append(index)
-
-        if anchor_wv.type == 'sense' and target.shifted_word not in anchor_wv.terms_with_sense:
-            print(f'{target.word} senses missing from {anchor_wv.corpus_name}')
+        if check_target_in_wv(anchor_wv, target.shifted_word):
             remove_targets.append(index)
             continue
-        elif anchor_wv.type == 'normal' and target.shifted_word not in anchor_wv.normal_vec.words:
-            print(f'{target.shifted_word} ({target.word}) missing from {anchor_wv.corpus_name}')
+
+        if anchor_wv.type == 'sense' and target.shifted_word not in anchor_wv.terms_with_sense:
+            print(f'Check 2: {target.word} senses missing from {anchor_wv.corpus_name}')
             remove_targets.append(index)
-
-
+        elif anchor_wv.type == 'normal' and target.shifted_word not in anchor_wv.normal_vec.words:
+            print(f'Check 2: {target.shifted_word} ({target.word}) missing from {anchor_wv.corpus_name}')
+            remove_targets.append(index)
 
     for index in sorted(remove_targets, reverse=True):
         # print(f'Deleting {targets[index].word}')
@@ -93,3 +103,12 @@ def filter_targets(
 
     return targets
 
+Target_Info = namedtuple('Target', ['word', 'shifted_word', 'is_shifted'])
+
+class Word_Pairs:
+    def __init__(self, all_wps=None, target_wps=None, target_labels=None):
+
+        self.all_wps = all_wps 
+        self.target_wps = target_wps
+        self.target_labels = target_labels
+ 
